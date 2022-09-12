60c5931b0a21754adf62830cafb4434bfcf2db0e
leominig <leonardominig@gmail.com>
2022-09-12 12:45:53 -0300

    first commit

---
+++ components/AlbumHero.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <section class="page-header">
+    <h2>
+      <NuxtLink to="/"> Back to List </NuxtLink>
+    </h2>
+  </section>
+</template>
+
+<style scoped></style>
---
+++ pages/albums/[id].vue
@@ -0,0 +1,40 @@
+<script setup>
+const route = useRoute();
+const albumStore = useAlbumStore();
+
+const { data: album } = await useAsyncData(
+  `album${route.params.id}`,
+  async () => {
+    if (route.params.id === "undefined") return albumStore.singleAlbum;
+    await albumStore.fetchAlbum(route.params.id);
+    return albumStore.singleAlbum;
+  },
+  {
+    pick: ["fields", "sys"],
+  }
+);
+
+useAsyncData("moreAlbums", async () => albumStore.fetchAdditionalAlbums());
+
+</script>
+<template>
+  <div class="page">
+    <AlbumHero />
+    <section class="page-content" v-if="album">
+      <article>
+        <AlbumPageHeader :album="album" />
+      </article>
+      <hr />
+      <div v-if="albumStore.moreAlbums" class="album-container container">
+        <AlbumCard
+          v-for="album in albumStore.moreAlbums"
+          :album="album"
+          :key="album.sys.id"
+          class="album"
+        />
+      </div>
+      <section />
+    </section>
+    <Footer />
+  </div>
+</template>
